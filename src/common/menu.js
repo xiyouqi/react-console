be7e435b5f05b4e47d9b5812a2da70e401eed463
fingnet <fingnet@gmail.com>
2018-04-12 15:18:06 +0800

    Initial commit

---
+++ src/common/menu.js
@@ -0,0 +1,142 @@
+import { isUrl } from '../utils/utils';
+
+const menuData = [
+  {
+    name: '工作台',
+    icon: 'dashboard',
+    path: 'dashboard/workplace',
+  },
+  {
+    name: '项目',
+    icon: 'form',
+    path: 'projects/basic-list',
+  },
+  {
+    name: '文档',
+    icon: 'table',
+    path: 'list',
+    children: [
+      {
+        name: '查询表格',
+        path: 'table-list',
+      },
+      {
+        name: '卡片列表',
+        path: 'card-list',
+      },
+      {
+        name: '搜索列表',
+        path: 'search',
+        children: [
+          {
+            name: '搜索列表（文章）',
+            path: 'articles',
+          },
+          {
+            name: '搜索列表（项目）',
+            path: 'projects',
+          },
+          {
+            name: '搜索列表（应用）',
+            path: 'applications',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '报表',
+    icon: 'profile',
+    path: 'profile',
+    children: [
+      {
+        name: '基础详情页',
+        path: 'basic',
+      },
+      {
+        name: '高级详情页',
+        path: 'advanced',
+        authority: 'admin',
+      },
+    ],
+  },
+  {
+    name: '数据',
+    icon: 'check-circle-o',
+    path: 'result',
+    children: [
+      {
+        name: '成功',
+        path: 'success',
+      },
+      {
+        name: '失败',
+        path: 'fail',
+      },
+    ],
+  },
+  {
+    name: '设置',
+    icon: 'warning',
+    path: 'exception',
+    children: [
+      {
+        name: '403',
+        path: '403',
+      },
+      {
+        name: '404',
+        path: '404',
+      },
+      {
+        name: '500',
+        path: '500',
+      },
+      {
+        name: '触发异常',
+        path: 'trigger',
+        hideInMenu: true,
+      },
+    ],
+  },
+  {
+    name: '账户',
+    icon: 'user',
+    path: 'user',
+    authority: 'guest',
+    children: [
+      {
+        name: '登录',
+        path: 'login',
+      },
+      {
+        name: '注册',
+        path: 'register',
+      },
+      {
+        name: '注册结果',
+        path: 'register-result',
+      },
+    ],
+  },
+];
+
+function formatter(data, parentPath = '/', parentAuthority) {
+  return data.map(item => {
+    let { path } = item;
+    if (!isUrl(path)) {
+      path = parentPath + item.path;
+    }
+    const result = {
+      ...item,
+      path,
+      authority: item.authority || parentAuthority,
+    };
+    if (item.children) {
+      result.children = formatter(item.children, `${parentPath}${item.path}/`, item.authority);
+    }
+    return result;
+  });
+}
+
+export const getMenuData = () => formatter(menuData);
