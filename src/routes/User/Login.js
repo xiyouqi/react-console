05d7c5010e691884f8f9113645951ff7118251bf
fingnet <fingnet@gmail.com>
2018-04-21 11:33:35 +0800

    Add Dockerfile

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -75,15 +75,6 @@ export default class LoginPage extends Component {
             </a>
           </div>
           <Submit loading={submitting}>登录</Submit>
-          <div className={styles.other}>
-            其他登录方式
-            <Icon className={styles.icon} type="alipay-circle" />
-            <Icon className={styles.icon} type="taobao-circle" />
-            <Icon className={styles.icon} type="weibo-circle" />
-            <Link className={styles.register} to="/user/register">
-              注册账户
-            </Link>
-          </div>
         </Login>
       </div>
     );
