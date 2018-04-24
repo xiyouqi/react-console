ca92646d0ca02999abfb726c255a7b1065582fdf
fuchunyan <fcy19930325>
2018-04-24 15:27:55 +0800

    项目详情-内容页面完善

--- src/routes/Project/ProjectContent.js
+++ src/routes/Project/ProjectContent.js
@@ -8,61 +8,107 @@ import styles from './ProjectContent.less';
   rule,
   loading: loading.models.rule,
 }))
+
 export default class VendorList extends PureComponent {
   state = {};
-  data = [
-    {
+  
+
+  render() {
+    const columns = [{
+      title: '任务',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: '项目阶段',
+      dataIndex: 'stage',
+      key: 'stage',
+      width: '12%',
+    }, {
+      title: '开始时间',
+      dataIndex: 'startTime',
+      width: '30%',
+      key: 'startTime',
+    }, {
+      title: '结束时间',
+      dataIndex: 'endTime',
+      width: '30%',
+      key: 'endTime',
+    }];
+    const data = [{
       key: 1,
       name: '任务1',
       stage: '阶段1',
       startTime: '2018-06-08',
-      endTime: '2018-08-08',
-      description: '任务1描述信息。',
-    },
-    {
+      endTime: '2019-06-08',
+      children: [{
+        key: 11,
+        name: '子任务1',
+        stage: '阶段1',
+        startTime: '2018-06-08',
+        endTime: '2018-08-08',
+      }, {
+        key: 12,
+        name: '子任务2',
+        stage: '阶段1',
+        startTime: '2018-08-08',
+        endTime: '2019-10-08',
+        children: [{
+          key: 121,
+          name: '子任务3',
+          stage: '阶段1',
+          startTime: '2018-08-08',
+          endTime: '2018-09-08',
+        }],
+      }, {
+        key: 13,
+        name: '子任务4',
+        stage: '阶段1',
+        startTime: '2018-10-08',
+        endTime: '2019-12-08',
+        children: [{
+          key: 131,
+          name: '子任务5',
+          stage: '阶段1',
+          startTime: '2018-10-08',
+          endTime: '2018-11-08',
+          children: [{
+            key: 1311,
+            name: '子任务6',
+            stage: '阶段1',
+            startTime: '2018-11-08',
+            endTime: '2018-11-18',
+          }, {
+            key: 1312,
+            name: '子任务7',
+            stage: '阶段1',
+            startTime: '2018-11-18',
+            endTime: '2018-12-08',
+          }],
+        }],
+      }],
+    }, {
       key: 2,
       name: '任务2',
       stage: '阶段2',
-      startTime: '2018-08-08',
-      endTime: '2018-10-08',
-      description: '任务2描述信息。',
-    },
-    {
-      key: 3,
-      name: '任务3',
-      stage: '阶段3',
-      startTime: '2018-10-08',
-      endTime: '2018-12-08',
-      description: '任务3描述信息。',
-    },
-  ];
-
-  render() {
-    const columns = [
-      { title: '任务', dataIndex: 'name', key: 'name' },
-      { title: '项目阶段', dataIndex: 'stage', key: 'stage' },
-      { title: '开始时间', dataIndex: 'startTime', key: 'startTime' },
-      { title: '结束时间', dataIndex: 'endTime', key: 'endTime' },
-      {
-        title: '操作',
-        dataIndex: '',
-        key: 'x',
-        render: () => (
-          <a href="" className={styles.deleteBtn}>
-            删除
-          </a>
-        ),
+      startTime: '2019-01-18',
+      endTime: '2010-12-08',
+    }];
+    const rowSelection = {
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
       },
-    ];
+      onSelect: (record, selected, selectedRows) => {
+        console.log(record, selected, selectedRows);
+      },
+      onSelectAll: (selected, selectedRows, changeRows) => {
+        console.log(selected, selectedRows, changeRows);
+      },
+    };
 
     return (
       <Card bordered={false} title="内容列表">
         <div className={styles.tableList}>
-          <Table
-            columns={columns}
-            expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
-            dataSource={this.data}
-          />
+          <Table columns={columns} rowSelection={rowSelection} dataSource={data} />
         </div>
       </Card>
     );
