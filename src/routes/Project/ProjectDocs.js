72b6d1404008156be629575b0c85a4b1bf298a5d
fingnet <fingnet@gmail.com>
2018-05-06 17:17:19 +0800

    Update

--- src/routes/Project/ProjectDocs.js
+++ src/routes/Project/ProjectDocs.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import moment from 'moment';
 import { connect } from 'dva';
-import { List, Card, Radio, Button, Icon, Dropdown, Menu, Avatar } from 'antd';
+import { List, Card, Radio, Button, Icon, Dropdown, Menu, Avatar, Tag } from 'antd';
 
 import styles from '../Documention/DocList.less';
 
@@ -112,6 +112,9 @@ export default class ProjectDocs extends PureComponent {
                   title={<a href={item.href}>{item.doc_title}</a>}
                   description={item.subDescription}
                 />
+                <div className={styles.projectTag}>
+                  {item.doc_tag.split(',').map(tag => <Tag>{tag}</Tag>)}
+                </div>
                 <ListContent data={item} />
               </List.Item>
             )}
